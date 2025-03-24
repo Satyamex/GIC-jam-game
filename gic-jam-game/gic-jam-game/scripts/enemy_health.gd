e866c2b79e85cf6850f6a6945886a917f69a7860
indieAYUSH <bhupeshgamedev8777@gmail.com>
2025-03-24 12:42:45 +0530

    s

--- gic-jam-game/gic-jam-game/scripts/enemy_health.gd
+++ gic-jam-game/gic-jam-game/scripts/enemy_health.gd
@@ -5,7 +5,7 @@ extends CharacterBody3D
 func _damage(damge:float):
 	health -= damge
 	if health <= 0:
-		get_parent().queue_free()
+		queue_free()
 
 func _physics_process(delta):
 	if !is_on_floor():

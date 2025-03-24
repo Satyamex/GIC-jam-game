90d50bafee7d36ed587c94e4e22dc7af678a5a8e
indieAYUSH <bhupeshgamedev8777@gmail.com>
2025-03-24 21:23:05 +0530

    ,,

--- gic-jam-game/gic-jam-game/levels/scripts/door_exit.gd
+++ gic-jam-game/gic-jam-game/levels/scripts/door_exit.gd
@@ -1,40 +1,28 @@
 extends Area3D
 
-@export var player_health: float = randf_range(150, 220)
 @export var locked: bool = true
 
 var _door_state: String = "true"
 @onready var s_open = $open
 @onready var s_close = $close
+@onready var blst_particles = $blst_particles
+
 @onready var animation_player = $AnimationPlayer
 
-func _ready():
-	randomize()
-	# Debug: Ensure the AnimationPlayer is found.
-	if animation_player == null:
-		print("AnimationPlayer node not found. Check the node path!")
+
 
 func open():
-	if locked:
-		_door_state = "locked"
-		print("Door is locked")
-	else:
-		print("Door is unlocked, playing open animation")
-		print("opening_ejfjakj")
-		$AnimationPlayer.play("open")
-		_door_state = "open"
-		_door_state = "locked"
-		#locked = true
+	pass
 
-func close():
-	if not locked:
-		_door_state = "locked"
-		print("Closing door")
-		animation_player.play("close")
+func _process(delta):
+	if Input.is_action_just_pressed("interaction"):
+		blst_particles.emitting =true
+		print("dfd")
+		animation_player.play()
+		self.queue_free()
 
 func _update_door_state(enemy_count: int) -> void:
 	if enemy_count <= 0 and locked:  # Only unlock once
-		locked = false
-		print("All enemies defeated. Door unlocked!")
-		print("locked =", locked)
-		open()
+		$blst_particles.emitting=true
+		$"blast\\".play()
+		self.queue_free()
